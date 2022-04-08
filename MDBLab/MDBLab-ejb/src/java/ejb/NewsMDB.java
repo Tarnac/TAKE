b7be3854b3721517873f62c46a4514439a5c7f2e
Tarnacki <tarnac97@gmail.com>
2022-04-08 14:24:02 +0200

    Lab05 - Message-Driven Beans

---
+++ MDBLab/MDBLab-ejb/src/java/ejb/NewsMDB.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/MessageDrivenBean.java to edit this template
+ */
+package ejb;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSDestinationDefinition;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+import javax.jms.TextMessage;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Tarnacki
+ */
+@JMSDestinationDefinition(name = "java:app/jms/NewsQueue", interfaceName = "javax.jms.Queue", resourceAdapter = "jmsra", destinationName = "NewsQueue")
+@MessageDriven(activationConfig = {
+    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "java:app/jms/NewsQueue"),
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+})
+public class NewsMDB implements MessageListener {
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    public NewsMDB() {
+    }
+    
+    @Override
+    public void onMessage(Message message) {
+        try {
+            if (message instanceof TextMessage) {
+                String[] msg = (((TextMessage) message).getText()).split("\\|");
+                NewsItem e = new NewsItem();
+                e.setHeading(msg[0]);
+                e.setBody(msg[1]);
+                em.persist(e);
+            }
+        } catch (JMSException e) {
+                e.printStackTrace();
+ }
+
+    }
+    
+}

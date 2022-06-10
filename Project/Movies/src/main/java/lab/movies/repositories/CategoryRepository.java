8d737dd5bf9fee7fa6d3ec4be4ea8dc3ecf23219
Tarnac <44372944+Tarnac@users.noreply.github.com>
2022-06-10 22:54:52 +0200

    Delete Project directory

--- Project/Movies/src/main/java/lab/movies/repositories/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package lab.movies.repositories;
-
-import lab.movies.models.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- Project/Movies/src/main/java/lab/movies/repositories/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package lab.movies.repositories;
-
-import lab.movies.models.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
